6e4cb810057c0a27557bf14ac47fb020292f1274
sunie <1287495769@qq.com>
2022-07-20 18:26:57 +0800

    add vertical

--- LineExtraction/extractor.py
+++ LineExtraction/extractor.py
@@ -1,3 +1,4 @@
+import enum
 from PIL import Image
 import numpy as np
 from tqdm import tqdm
@@ -16,48 +17,68 @@ class Extractor:
         img = img.convert('RGB')   # 修改颜色通道为RGB
         x, y = img.size   # 获得长和宽
 
-        self.points = []
+        points = []
         for i in tqdm(range(x)):
             for k in range(y):
                 pixel = img.getpixel((i, k))
                 if np.sum(pixel) > 50:
-                    if self.points:
-                        last = self.points[-1]
-                        offset = i - last[0]
-                        # 单调递减
-                        if k - last[1] >= 0:
-                            self.points.append((i, k))
-                            break
-                    else:
-                        self.points.append((i, k))
+                    if not points or points and k >= points[-1][1]:
+                        points.append((i, k))
                         break
+        # print(points)
 
-        # print("points", self.points)
-        # self.color_img = Image.new('RGB', (x, y), 'black')
-        # for p in self.points:
-        #     self.color_img.putpixel(p, img.getpixel(p))
-        # self.color_img.show()
-
-        
-
-        self.cordinate = []
-        for p in self.points:
+        cordinate = []
+        for ix, p in enumerate(points):
             xx = round(p[0]/x*(xt-xs) + xs, 3)
             yy = round((1-p[1]/y)*(yt-ys) + ys, 3)
-            if len(self.cordinate) and xx - self.cordinate[-1][0] < 1:
+            if ix and xx - cordinate[-1][0] < 1:
                 continue
-            self.cordinate.append((xx, yy))
+            cordinate.append((xx, yy))
+
+
+        y_cor = [p[1] for p in points]
+        points = []
+        for k in tqdm(range(y-1, 0, -1)):
+            for i in range(x-1, 0, -1):
+                pixel = img.getpixel((i, k))
+                if np.sum(pixel) > 50 and k not in y_cor:
+                    if not points or points and points[-1][0] >= i:
+                        points.append((i, k))
+                        break
+        # print(points)
+
+        for ix, p in enumerate(points):
+            xx = round(p[0]/x*(xt-xs) + xs, 3)
+            yy = round((1-p[1]/y)*(yt-ys) + ys, 3)
+            if ix and cordinate[-1][1] - yy > 1:
+                continue
+            cordinate.append((xx, yy))
+        
+        def get_x(p):
+            return (p[0] * 100000 - p[1]) / 100000
+        def distance(x, y):
+            return (x[0] - y[0]) ** 2 + (x[1] - y[1]) ** 2
+        
+        cordinate.sort(key=get_x)
+
+        self.cordinate = []
+        for c in cordinate:
+            if self.cordinate and distance(self.cordinate[-1], c) > 5:
+                self.cordinate.append(c)
             
         csv = generate_name('.csv')
         print(f"曲线坐标点已保存在：{csv}")
         with open(csv, 'w') as f:
             f.write("x,y\n")
-            for p in self.cordinate:
+            for p in cordinate:
                 f.write(f"{p[0]},{p[1]}\n")
 
-        self.x = [p[0] for p in self.cordinate]
-        self.y = [p[1] for p in self.cordinate]
+        self.x = [get_x(p) for p in cordinate]
+        self.y = [p[1] for p in cordinate]
 
+        # print("cor", cordinate)
+        # print(self.x)
+        # print(self.y)
     
     def fit(self):
         '''拟合'''
@@ -78,7 +99,7 @@ class Extractor:
         pl.figure(figsize=(7, 7))
         pl.plot(self.x, self.y,'ro')
         xnew = np.linspace(min(self.x), max(self.x), 1000)
-        for kind in ['nearest', 'zero','linear','quadratic', 'cubic']:
+        for kind in ['nearest', 'zero','linear']: # ,'quadratic', 'cubic'
             #根据kind创建插值对象interp1d
             f = interpolate.interp1d(self.x, self.y, kind = kind)
             ynew = f(xnew)      #计算插值结果
